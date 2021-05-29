68f00db0ae40eb97f1350a785d89625f5644986f
Umang01-hash <mundhraumang.02@gmail.com>
2021-05-29 23:45:05 +0530

    Show all users

--- models/userModels.js
+++ models/userModels.js
@@ -11,6 +11,12 @@ const userSchema = mongoose.Schema({
       created: { type: Date, default: Date.now() },
     },
   ],
+  following: [
+    {userFollowed : { type : mongoose.Schema.Types.ObjectId, ref: 'User'} }
+  ],
+  followers : [
+    {follower : { type: mongoose.Schema.Types.ObjectId , ref: 'User'} }
+  ]
 });
 
 module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -54,10 +54,12 @@ require('./socket/streams')(io);
 const auth = require("./routes/authRoutes");
 const posts = require('./routes/postRoutes');
 const users = require('./routes/userRoutes');
+const friends = require('./routes/friendsRoute');
 
 app.use("/api/chatapp", auth);
 app.use('/api/chatapp', posts);
 app.use('/api/chatapp',users);
+app.use('/api/chatapp', friends);
 
 server.listen(3000, () => {
   console.log("Running on Port 3000");
