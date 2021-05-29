c35084926dce58cf524aeeed1c72cedc0e58acca
Umang01-hash <mundhraumang.02@gmail.com>
2021-05-30 01:54:17 +0530

    Unfollow Button working

--- controllers/friends.js
+++ controllers/friends.js
@@ -38,4 +38,40 @@ module.exports = {
         res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message : 'Error Occured' })
       });
   },
+
+  UnFollowUser(req, res) {
+    const unFollowUser = async () => {
+        await User.updateOne({
+            _id : req.user._id,
+
+        },
+        {
+            $pull : {
+                following : {
+                    userFollowed : req.body.userFollowed
+                }
+            }
+        } );
+
+        await User.updateOne({
+            _id : req.user.userFollowed,
+
+        },
+        {
+            $pull : {
+                followers : {
+                    follower : req.user._id
+                }
+            }
+        } );
+    };
+
+    unFollowUser()
+      .then(() => {
+          res.status(HttpStatus.StatusCodes.OK).json({ message : 'UnFollowing user now' })
+      })
+      .catch((err) => {
+        res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message : 'Error Occured' })
+      });
+  },
 };
