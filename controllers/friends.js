3d440e20f32abbe46a1e181ca334ee8bc47d89f6
Umang01-hash <mundhraumang.02@gmail.com>
2021-05-29 23:50:43 +0530

    Follow functionality partially done

---
+++ controllers/friends.js
@@ -0,0 +1,41 @@
+const HttpStatus = require("http-status-codes");
+
+const User = require("../models/userModels");
+
+module.exports = {
+  FollowUser(req, res) {
+    const followUser = async () => {
+        await User.updateOne({
+            _id : req.user._id,
+            'following.userFollowed' : { $ne: req.body.userFollowed }
+        },
+        {
+            $push : {
+                following : {
+                    userFollowed : req.body.userFollowed
+                }
+            }
+        } );
+
+        await User.updateOne({
+            _id : req.user.userFollowed,
+            'following.follower' : { $ne: req.user._id }
+        },
+        {
+            $push : {
+                followers : {
+                    follower : req.user._id
+                }
+            }
+        } );
+    };
+
+    followUser()
+      .then(() => {
+          res.status(HttpStatus.StatusCodes.OK).json({ message : 'Following user now' })
+      })
+      .catch((err) => {
+        res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message : 'Error Occured' })
+      });
+  },
+};
