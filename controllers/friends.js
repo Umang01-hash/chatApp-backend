621b295e9c88b786c042f603a4b7cfede8df881c
Umang01-hash <mundhraumang.02@gmail.com>
2021-05-30 15:23:40 +0530

    Notifications appearing

--- controllers/friends.js
+++ controllers/friends.js
@@ -3,75 +3,91 @@ const HttpStatus = require("http-status-codes");
 const User = require("../models/userModels");
 
 module.exports = {
-  FollowUser(req, res) {
-    const followUser = async () => {
-        await User.updateOne({
-            _id : req.user._id,
-            'following.userFollowed' : { $ne: req.body.userFollowed }
-        },
-        {
-            $push : {
-                following : {
-                    userFollowed : req.body.userFollowed
+    FollowUser(req, res) {
+        const followUser = async () => {
+          await User.updateOne(
+            {
+              _id: req.user._id,
+              'following.userFollowed': { $ne: req.body.userFollowed }
+            },
+            {
+              $push: {
+                following: {
+                  userFollowed: req.body.userFollowed
                 }
+              }
             }
-        } );
+          );
 
-        await User.updateOne({
-            _id : req.user.userFollowed,
-            'following.follower' : { $ne: req.user._id }
-        },
-        {
-            $push : {
-                followers : {
-                    follower : req.user._id
+          await User.updateOne(
+            {
+              _id: req.body.userFollowed,
+              'following.follower': { $ne: req.user._id }
+            },
+            {
+              $push: {
+                followers: {
+                  follower: req.user._id
+                },
+                notifications: {
+                  senderId: req.user._id,
+                  message: `${req.user.username} is now following you.`,
+                  created: new Date(),
+                  viewProfile: false
                 }
+              }
             }
-        } );
-    };
+          );
+        };
 
-    followUser()
-      .then(() => {
-          res.status(HttpStatus.StatusCodes.OK).json({ message : 'Following user now' })
-      })
-      .catch((err) => {
-        res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message : 'Error Occured' })
-      });
-  },
+        followUser()
+          .then(() => {
+            res.status(HttpStatus.StatusCodes.OK).json({ message: 'Following user now' });
+          })
+          .catch(err => {
+            res
+              .status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
+              .json({ message: 'Error occured' });
+          });
+      },
 
-  UnFollowUser(req, res) {
-    const unFollowUser = async () => {
-        await User.updateOne({
-            _id : req.user._id,
-
-        },
-        {
-            $pull : {
-                following : {
-                    userFollowed : req.body.userFollowed
+      UnFollowUser(req, res) {
+        const unFollowUser = async () => {
+          await User.updateOne(
+            {
+              _id: req.user._id
+            },
+            {
+              $pull: {
+                following: {
+                  userFollowed: req.body.userFollowed
                 }
+              }
             }
-        } );
+          );
 
-        await User.updateOne({
-            _id : req.user.userFollowed,
-
-        },
-        {
-            $pull : {
-                followers : {
-                    follower : req.user._id
+          await User.updateOne(
+            {
+              _id: req.body.userFollowed
+            },
+            {
+              $pull: {
+                followers: {
+                  follower: req.user._id
                 }
+              }
             }
-        } );
-    };
+          );
+        };
 
-    unFollowUser()
-      .then(() => {
-          res.status(HttpStatus.StatusCodes.OK).json({ message : 'UnFollowing user now' })
-      })
-      .catch((err) => {
-        res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message : 'Error Occured' })
-      });
-  },
+        unFollowUser()
+          .then(() => {
+            res.status(HttpStatus.StatusCodes.OK).json({ message: 'FUnfllowing user now' });
+          })
+          .catch(err => {
+            res
+              .status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
+              .json({ message: 'Error occured' });
+          });
+      },
 };
