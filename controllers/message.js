8a31725d5d0140d6d81c0e61857e400fc33ac133
Umang01-hash <mundhraumang.02@gmail.com>
2021-05-31 11:17:15 +0530

    private chat part 1

---
+++ controllers/message.js
@@ -0,0 +1,5 @@
+module.exports = {
+    SendMessage(req,res){
+            console.log(req.body);
+    }
+}
--- server.js
+++ server.js
@@ -55,11 +55,13 @@ const auth = require("./routes/authRoutes");
 const posts = require('./routes/postRoutes');
 const users = require('./routes/userRoutes');
 const friends = require('./routes/friendsRoute');
+const message=require('./routes/messageRoutes');
 
 app.use("/api/chatapp", auth);
 app.use('/api/chatapp', posts);
 app.use('/api/chatapp',users);
 app.use('/api/chatapp', friends);
+app.use('/api/chatapp', message);
 
 server.listen(3000, () => {
   console.log("Running on Port 3000");
